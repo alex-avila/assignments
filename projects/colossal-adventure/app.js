d8310db049fe74471bb18ab50113b96490d79052
alex-avila <alex.avilx@icloud.com>
2018-05-05 17:31:07 -0600

    Finished second iteration of RPG using the chalk npm package for colors

--- projects/colossal-adventure/app.js
+++ projects/colossal-adventure/app.js
@@ -1,6 +1,9 @@
 var ask = require('readline-sync');
+var chalk = require('chalk');
 
-// ALGORITHMS
+var log = console.log;
+
+// ALGORITHMS --> DONE
 function randomChance(odds) {
     return Math.floor(Math.random() * odds) === 0 ? true : false;
 }
@@ -22,10 +25,6 @@ function willMonsterAttack() {
     return randomChance(2);
 }
 
-function willTheyEscape() {
-    return randomChance(2);
-}
-
 function getAttackPower(attacker) {
     return random(30) + attacker.baseAttack;
 }
@@ -33,26 +32,81 @@ function getAttackPower(attacker) {
 
 // GAME STEPS
 function beginGame() {
-    console.log('Hello. This is a game.');
-    player.name = ask.question('What is your name? ');
-    console.log(`\nHello ${player.name}. You can walk or you can check your inventory [w/i]`);
+    log(chalk.bold('\nWELCOME TO POCKET MONSTERS: THE GAME'));
+    log('\nYou are a pocket monster. The yellow one.');
+    log('You\'ve just finished your shift filming for pokemon.');
+    log('You want to go home, but three prisoners have escaped nearby.');
+    log('\nYou will encounter them and have the choice to attack or run');
+    log('While nothing is happening, however, you will simply');
+    log('continue walking home. \n\nYou win when you get home safely\n');
+    player.name = ask.question('I forgot what you\'re name was though. Can you remind me?\n\n> ');
+    if (player.name.toUpperCase() != 'PIKACHU') {
+        log(`Hmm, I don't remember ${chalk.bold(player.name)} being your name. Anyway...`);
+        // Give common inventory
+        player.inventory = [
+            {
+                type: 'weapon',
+                name: 'A wooden sword',
+                description: 'The most basic sword you could possibly have'
+            },
+            {
+                type: 'sheild',
+                name: 'Tacky clothes',
+                description: 'Please change your outfit. It looks horrible and it has no effect'
+            }
+        ];
+    } else {
+        log(`Oh, right. how could I have forgotten that ${chalk.bold(player.name)} is your name.`);
+        // Give special inventory
+        player.inventory = [
+            {
+                type: 'weapon',
+                name: 'Electricity',
+                description: 'Since you are pikachu, you have electricity as an attack'
+            },
+            {
+                type: 'sheild',
+                name: 'Fur',
+                description: 'Your yellow fur protects you. Damaged taken is reduced by 12%'
+            }
+        ];
+    }
+    player.name = chalk.bold(player.name);
+    log('You can either choose to walk or check your inventory. [w/i]');
 }
 
 function endGame(how) {
     if (how === 'won') {
-        console.log('You won');
+        log(chalk.bgGreen.black.bold('You won'));
         endSession = true;
     } else if (how === 'death') {
-        console.log('You died');
+        log(chalk.bgRed.white.bold('You died'));
         player.isAlive = false;
     } else if (how === 'quit') {
-        console.log('Are you sure you want to quit? [y/N]')
+        log('Are you sure you want to quit? [y/N]')
         if (ask.question('\n\n> ').toUpperCase() === 'Y') {
             endSession = true;
         } else {
-            console.log('Let\'s continue then.')
+            log('Let\'s continue then.')
+        }
+    } else if (how === 'home') {
+        log('Oh, look. You\'re home.');
+        log(chalk.bgGreen.black.bold('You won'));
+        endSession = true;
+    }
+}
+
+function updateProgressBar() {
+    var progressBar = '|';
+    for (var i = 0; i < absDistance; i++) {
+        if (i === (location - 1)) {
+            progressBar += chalk.inverse('x');
+        } else {
+            progressBar += '-';
         }
     }
+    progressBar += '|';
+    return progressBar;
 }
 
 
@@ -66,32 +120,32 @@ var player = {
                 type: 'weapon',
                 name: 'A wooden sword',
                 description: 'The most basic sword you could possibly have'
-        },
+            },
             {
                 type: 'sheild',
                 name: 'Tacky clothes',
                 description: 'Please change your outfit. It looks horrible and it has no effect'
-        }
-    ],
+            }
+        ],
         isAlive: true
     },
     monsters = [
         {
-            name: 'Cthulu',
+            name: chalk.red('Cthulu'),
             hp: 50,
             baseAttack: 10,
             isAlive: true
         },
         {
-            name: 'Dementor #12',
-            hp: 25,
+            name: chalk.red('Dementor #12'),
+            hp: 30,
             baseAttack: 5,
             isAlive: true
         },
         {
-            name: 'Area X',
+            name: chalk.red('Area X'),
             hp: 100,
-            baseAttack: 15,
+            baseAttack: 20,
             isAlive: true
         }
     ],
@@ -117,7 +171,10 @@ var player = {
             description: 'Increases your attack power by 25%'
         }
     ],
-    endSession = false;
+    endSession = false,
+    absDistance = 10,
+    location = 0;
+//    progressLine = createProgressBar();
 
 // GOD ACTIONS
 function giveHPAndItem() {
@@ -138,27 +195,28 @@ function createMonster() {
 // ABSTRACTED ACTIONS
 function attack(victim, attacker) {
     attacker.attackPower = getAttackPower(attacker);
+    var printAttackPower = chalk.hex('#6c25de')(attacker.attackPower);
     victim.hp -= attacker.attackPower;
     if (victim.name === player.name) {
-        console.log(`Oh no, ${monster.name} attacked you for ${attacker.attackPower} points`);
+        log(`Oh no, ${attacker.name} attacked you for ${printAttackPower} points`);
     } else {
-        console.log(`You damaged ${victim.name} for ${attacker.attackPower} points`);
+        log(`You damaged ${victim.name} for ${printAttackPower} points`);
     }
     var survived = victim.hp > 0;
     if (!survived) {
         victim.hp = 0;
         if (victim.name === player.name) {
-            console.log(`${attacker.name} seems to have killed you.`);
+            log(`${attacker.name} seems to have killed you.`);
             endGame('death');
         } else {
-            console.log(`You killed ${victim.name}`);
+            log(`You killed ${victim.name}`);
             die(victim);
         }
     } else if (survived) {
         if (victim.name === player.name) {
-            console.log(`Your HP is now ${player.hp}`);
+            log(chalk`Your HP is now {green.bold ${player.hp}}`);
         } else {
-            console.log(`Its HP is now ${monster.hp}`);
+            log(chalk`Its HP is now {green ${monster.hp}}`);
         }
     }
 }
@@ -167,7 +225,7 @@ function die(who) {
     if (who.name != player.name) {
         previousHP = player.hp;
         giveHPAndItem();
-        console.log(`Oh look, your hp went up from ${previousHP} to ${player.hp}`);
+        log(chalk`Oh look, your hp went up from {green ${previousHP}} to {green.bold ${player.hp}}`);
     }
     who.isAlive = false;
 }
@@ -182,13 +240,13 @@ function walk() {
             if (willMonsterAttack()) {
                 attack(player, monster);
             } else {
-                console.log(`Oh no, ${monster.name} has appeared.`);
+                log(`Oh no, ${monster.name} has appeared.`);
             }
         } else if (!monsterAppeared) {
-            console.log('Just walk. Nothing else is happening.');
+            log('No prisoner appeared. You may continue.');
         }
     } else {
-        console.log('Looks like all monsters are dead');
+        log('Looks like all monsters are dead');
         endGame('won');
     }
 }
@@ -196,27 +254,27 @@ function walk() {
 function run() {
     var escaped = randomChance(2) ? true : false;
     if (escaped) {
-        console.log('You escaped. You can continue to walk now.');
+        log('You escaped. You can continue to walk now.');
     } else {
-        console.log(`You attempt to escape, but not before ${monster.name} has a chance to attack you.`);
+        log(`You attempt to escape, but not before ${monster.name} has a chance to attack you.`);
         attack(player, monster);
         if (player.isAlive) {
-            console.log('Luckily, you survived');
+            log('Luckily, you survived');
         }
     }
 }
 
 function checkInventory() {
     var inventory = 0;
-    console.log(`Name: ${player.name}\nHP: ${player.hp}`);
-    console.log('Inventory:');
-    player.inventory.forEach(item => console.log(`- ${item.name}`));
+    log(`Name: ${player.name}\nHP: ${player.hp}`);
+    log('Inventory:');
+    player.inventory.forEach(item => log(`- ${item.name}`));
 }
 
 var indecisionCount = 0;
 
 function confrontation() {
-    console.log('Run or attack? [r/a]');
+    log('Run or attack? [r/a]');
     choice = ask.question('\n\n> ');
     switch (choice.toUpperCase()) {
         case 'R':
@@ -225,10 +283,9 @@ function confrontation() {
             break;
         case 'A':
             indecisionCount = 0;
-            console.log(`You decided to attack ${monster.name}.`);
             attack(monster, player);
             if (monster.isAlive) {
-                console.log(`But ${monster.name} decided to attack you back.`);
+                log(`But ${monster.name} decided to attack you back.`);
                 attack(player, monster);
             }
             break;
@@ -237,11 +294,20 @@ function confrontation() {
             if (indecisionCount === 2) {
                 endGame('death');
             } else {
-                console.log('If you don\'t choose one of the options I\'m gonna kill you');
+                log('If you don\'t choose one of the options I\'m gonna kill you');
             }
     }
 }
 
+function updateLocation() {
+    location++;
+    if (location === absDistance) {
+        log(updateProgressBar());
+        endGame('home');
+    } else {
+        log(updateProgressBar());
+    }
+}
 
 beginGame();
 while (player.isAlive && !endSession) {
@@ -251,12 +317,19 @@ while (player.isAlive && !endSession) {
             var monsterAppeared = false;
             walk();
             if (monsterAppeared && player.isAlive) {
+                // if monster appeared do the following
                 confrontation();
                 if (indecisionCount != 0) {
                     confrontation();
                 } else if (player.isAlive) {
-                    console.log('\nKeep walking.');
+                    // If after the confrontation player is alive
+                    // update location
+                    log('Keep walking');
+                    updateLocation();
                 }
+            } else if (!monsterAppeared) {
+                // if monster didn't appear update location
+                updateLocation();
             }
             break;
         case 'I':
@@ -266,7 +339,7 @@ while (player.isAlive && !endSession) {
             endGame('quit');
             break;
         default:
-            console.log('Pardon?')
+            log('Pardon?')
             break;
     }
 }
