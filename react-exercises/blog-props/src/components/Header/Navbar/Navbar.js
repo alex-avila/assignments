88697efa11660a405efa7cbc895c0bd09a690213
alex-avila <alex.avilx@icloud.com>
2018-05-19 19:22:45 -0600

    Started animations on blog props and finished udacity portfolio

--- react-exercises/blog-props/src/components/Header/Navbar/Navbar.js
+++ react-exercises/blog-props/src/components/Header/Navbar/Navbar.js
@@ -1,7 +1,8 @@
 import React from 'react'
-import './Navbar.css'
-
 import NavList from './NavList/NavList'
+import Transition from 'react-transition-group/Transition';
+
+import './Navbar.css'
 
 const menuBreakpoint = 992;
 
@@ -21,7 +22,6 @@ const navStyles = {
         color: 'black',
         position: 'fixed',
         animation: 'slideIn 0.5s'
-        // top: 0
     }
 }
 
@@ -32,21 +32,22 @@ class Navbar extends React.Component {
             isToggleOn: false,
             isScreenBig: false,
             scrolledUp: false,
-            positionY: 0
+            positionY: 0,
+            in: true
         };
 
         // This binding is necessary to make 'this' work in the callback
-        this.toggleNavItems = this.toggleNavItems.bind(this);
+        this.handleClick = this.handleClick.bind(this);
         this.handleResize = this.handleResize.bind(this);
         this.handleScroll = this.handleScroll.bind(this);
     }
 
-    toggleNavItems() {
+    handleClick() {
         this.setState(prevState => ({
             isToggleOn: !prevState.isToggleOn
         }));
     }
-    
+
     handleResize() {
         this.setState(prevState => ({
             isScreenBig: window.innerWidth >= menuBreakpoint ? true : false,
@@ -75,41 +76,52 @@ class Navbar extends React.Component {
         window.addEventListener('scroll', null);
     }
 
+    // Determine how navbar should render
+    determinNavView() {
+        if (window.scrollY === 0 && this.state.isScreenBig) {
+            return navStyles.large
+        }
+        if (!this.state.isScreenBig) {
+            return navStyles.small
+        } else if (this.state.isScreenBig && this.state.scrolledUp) {
+            return navStyles.largeTransluscent
+        } else {
+            return navStyles.large
+        }
+    }
+
+    toggleEnteredState() {
+        this.setState(prevState => ({
+            in: !prevState.in
+        }))
+    }
 
     render() {
-        let navWrapperClass = 'nav__wrapper';
-        if (this.state.isScreenBig) {
-            navWrapperClass += ' nav__wrapper__flex';
-        }
-        // Right here determine how navbar should render
-        let passToNav = navStyles.large;
-        if (!this.state.isScreenBig && this.state.isToggleOn) {
-            passToNav = navStyles.small
-        } else if (this.state.isScreenBig && this.state.scrolledUp) {
-            passToNav = navStyles.largeTransluscent
-        }
-        if (window.scrollY === 0) {
-            passToNav = navStyles.large
-        }
+        let navWrapperClass = this.state.isScreenBig ?
+            'nav__wrapper nav__wrapper__flex' : 'nav__wrapper';
         return (
-            <nav style={passToNav}>
-                <div className={navWrapperClass}>
-                    <div className="nav__with-button">
-                        <h1>Start Bootstrap</h1>
-                        {
-                            !this.state.isScreenBig &&
-                            <div className="button" onClick={this.toggleNavItems}>
-                                <span className="button__text">MENU</span>
-                                <span className="button__icon"></span>
+            <Transition in={this.state.entered} timeout={250}>
+                {(state) =>(
+                    <nav style={this.determinNavView()}>
+                        <div className={navWrapperClass}>
+                            <div className="nav__with-button">
+                                <h1>Start Bootstrap</h1>
+                                {
+                                    !this.state.isScreenBig &&
+                                    <div className="button" onClick={this.handleClick}>
+                                        <span className="button__text">MENU</span>
+                                        <span className="button__icon"></span>
+                                    </div>
+                                }
                             </div>
-                        }
-                    </div>
-                    {
-                        (this.state.isToggleOn || this.state.isScreenBig) &&
-                        <NavList isScreenBig={this.state.isScreenBig}/>
-                    }
-                </div>
-            </nav>
+                            {
+                                (this.state.isToggleOn || this.state.isScreenBig) &&
+                                <NavList isScreenBig={this.state.isScreenBig} />
+                            }
+                        </div>
+                    </nav>
+                )}
+            </Transition>
         );
     }
 }
