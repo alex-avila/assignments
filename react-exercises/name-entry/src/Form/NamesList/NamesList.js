7553382261fd5591b0c2a0af2c714491e5f7b763
alex-avila <alex.avilx@icloud.com>
2018-05-21 12:10:43 -0600

    Removed one unused material ui component

--- react-exercises/name-entry/src/Form/NamesList/NamesList.js
+++ react-exercises/name-entry/src/Form/NamesList/NamesList.js
@@ -1,7 +1,6 @@
 import React from 'react'
 
 import List from '@material-ui/core/List';
-import ListItem from '@material-ui/core/ListItem';
 import ListItemText from '@material-ui/core/ListItemText';
 import 'typeface-roboto';
 
