9e5e445813d5c14d75197e40f0e8ced50f0a7330
alex-avila <alex.avilx@icloud.com>
2018-05-30 18:33:45 -0600

    Added base styles to Navbar

--- projects/personal-react-project/src/components/Navbar/Navbar.js
+++ projects/personal-react-project/src/components/Navbar/Navbar.js
@@ -2,16 +2,20 @@ import React, { Component } from 'react';
 
 import { NavLink } from 'react-router-dom'
 
+import './Navbar.css'
+
 class Navbar extends Component {
     render() {
         return (
-            <div>
-                <nav>
-                    <NavLink exact to="/">Weather</NavLink>
-                    <NavLink exact to="/News">News</NavLink>
-                    <NavLink exact to="/Game">Game</NavLink>
-                </nav>
-            </div>
+            <nav className="nav__wrapper--outer">
+                <div className="nav__wrapper--inner">
+                    <div className="nav__links">
+                        <NavLink exact to="/">Weather</NavLink>
+                        <NavLink exact to="/News">News</NavLink>
+                        <NavLink exact to="/Game">Game</NavLink>
+                    </div>
+                </div>
+            </nav>
         );
     }
 }
