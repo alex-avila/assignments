0c21f3ef17101accee2a704ae89aeea6acd10fc4
alex-avila <alex.avilx@icloud.com>
2018-06-06 10:30:07 -0600

    Fixed PUT request to accept new uuid's

--- express-exercises/original-bounty-hunter/server/routes/bountyRoutes.js
+++ express-exercises/original-bounty-hunter/server/routes/bountyRoutes.js
@@ -14,14 +14,15 @@ bountyRouter.route('/')
 bountyRouter.route('/:id')
     .get((req, res) => res.send(bounties.find(bounty => bounty.id === Number(req.params.id))))
     .put((req, res) => {
-        bountyIndex = bounties.indexOf(bounties.find(bounty => bounty.id === Number(req.params.id)))
+        bountyIndex = bounties.findIndex(bounty => bounty.id == req.params.id)
         Object.keys(req.body).forEach(key => {
             bounties[bountyIndex][key] = req.body[key]
         })
+        console.log(bounties[bountyIndex])
         res.send(bounties[bountyIndex])
     })
     .delete(((req, res) => {
-        bounties.splice(bounties.indexOf(bounties.find(bounty => bounty.id === Number(req.params.id))), 1)
+        bounties.splice(bounties.findIndex(bounty => bounty.id === Number(req.params.id)), 1)
         res.send(bounties)
     }))
 
