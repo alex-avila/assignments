0b6f31b86bc2e97e2c75a8a7dffe478f610831d5
alex-avila <alex.avilx@icloud.com>
2018-06-06 10:41:23 -0600

    refactored PUT request to use object.assign

--- express-exercises/original-bounty-hunter/server/routes/bountyRoutes.js
+++ express-exercises/original-bounty-hunter/server/routes/bountyRoutes.js
@@ -14,12 +14,9 @@ bountyRouter.route('/')
 bountyRouter.route('/:id')
     .get((req, res) => res.send(bounties.find(bounty => bounty.id === Number(req.params.id))))
     .put((req, res) => {
-        bountyIndex = bounties.findIndex(bounty => bounty.id == req.params.id)
-        Object.keys(req.body).forEach(key => {
-            bounties[bountyIndex][key] = req.body[key]
-        })
-        console.log(bounties[bountyIndex])
-        res.send(bounties[bountyIndex])
+        const bounty = bounties.find(bounty => bounty.id == req.params.id)
+        Object.assign(bounty, req.body)
+        res.send(bounty)
     })
     .delete(((req, res) => {
         bounties.splice(bounties.findIndex(bounty => bounty.id === Number(req.params.id)), 1)
