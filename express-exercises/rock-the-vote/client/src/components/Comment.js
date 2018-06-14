ab0e9b2ec1d6e517f9225b328319081fdba69924
alex-avila <alex.avilx@icloud.com>
2018-06-14 17:30:11 -0600

    Removed unnecessary code

--- express-exercises/rock-the-vote/client/src/components/Comment.js
+++ express-exercises/rock-the-vote/client/src/components/Comment.js
@@ -14,17 +14,10 @@ class Comment extends Component {
     }
 
     render() {
-        const { content, createdAt, updatedAt, _id: id, issue } = this.props
+        const { content, _id: id, issue } = this.props
         return (
             <div id={id} className="comment">
                 <p>{content}</p>
-                {/* <p><small>
-                    {
-                        updatedAt ? 
-                            new Date(updatedAt).toLocaleDateString() :
-                            new Date(createdAt).toLocaleDateString()
-                    }
-                </small></p> */}
                 <button onClick={() => this.handleClick(issue, id)}>x</button>
             </div>
         )
