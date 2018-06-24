f0b4b9431156417e4fbd3f33a75a8adffb66950a
alex-avila <alex.avilx@icloud.com>
2018-06-24 13:24:55 -0600

    Simplify Home component

--- projects/full-stack-app/client/src/scenes/Home/index.js
+++ projects/full-stack-app/client/src/scenes/Home/index.js
@@ -15,25 +15,7 @@ class Decks extends Component {
     render() {
         const { decks } = this.props
         const mappedDecks = decks
-            .map(deck => {
-                const { _id, inQueue: { cards } } = deck
-                const cardObject = cards.reduce((final, card) => {
-                    if (card.hasBeenSeen) {
-                        final.newCards.push(card)
-                    } else {
-                        final.reviews.push(card)
-                    }
-                    return final
-                }, { newCards: [], reviews: [] })
-                const newCards = cardObject.newCards.slice(0, deck.settings.newCards.perDay)
-                const reviews = cardObject.reviews.slice(0, deck.settings.reviews.perDay)
-                const availableCards = [...newCards, ...reviews]
-                // Add condition to check if it was created today
-                if (new Date(deck.today).getDate() !== new Date(Date.now()).getDate()) {
-                    this.props.getCardsPerDeck(card._id, availableCards)
-                }
-                return <Deck key={_id} {...deck} availableCards={availableCards} />
-            })
+            .map(deck => <Deck key={deck._id} {...deck} />)
         return (
             <section className="decks-list">
                 <h2 className="utility-wrapper">Decks</h2>
@@ -45,4 +27,4 @@ class Decks extends Component {
     }
 }
 
-export default connect(state => ({ decks: state.deckData.decks }), { getDecks, getC })(Decks);
+export default connect(state => ({ decks: state.deckData.decks }), { getDecks })(Decks);
